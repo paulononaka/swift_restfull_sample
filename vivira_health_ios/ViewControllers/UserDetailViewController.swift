ede2af1a3b8792a75fa4e183687c8d492db8a0f6
Paulo Henrique Nonaka <paulononaka@gmail.com>
2016-01-15 14:27:13 -0200

    creating StoryBoard UserDetail, UserDetailViewController and adding user image

---
+++ vivira_health_ios/ViewControllers/UserDetailViewController.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
